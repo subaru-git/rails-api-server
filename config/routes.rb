be01025accbb52f3033ba1d07bfddece12ff241e
subaru-git <subaru12081985@yahoo.co.jp>
2018-10-03 22:37:46 +0900

    change the folder structure of controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope :api do
-    scope :v1 do
-      scope :articles do
+  namespace :api do
+    namespace :v1 do
+      namespace :articles do
         post "/", to: "article#new"
       end
     end

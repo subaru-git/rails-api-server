afe888c74b2ca3a6d5cebc2b30e2bcf23c985ec8
subaru-git <subaru12081985@yahoo.co.jp>
2018-10-11 22:58:19 +0900

    add like model validates

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe Like, type: :model do
     it "if set like" do
       expect(@like.valid?).to eq(true)
     end
+
+    it "if like is already created" do
+      @like.save
+      samelike = build(:like)
+      samelike.user = @like.user
+      expect(samelike.valid?).to eq(false)
+    end
   end
 end
